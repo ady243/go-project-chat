b90b8a22df52fd96ade9dc2129980b1b5717741c
Ady Antony <adymasivi@MacBook-Pro-de-Ady.local>
2023-06-05 12:00:48 +0200

    essaie de run le server en demo

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, World!")
+	})
+
+	fmt.Println("Server started on http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
 }
