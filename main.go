b0f575be758c6dacb17ecb922b7906171ede07e8
ady243 <masiviady7@gmail.com>
2023-06-06 14:21:32 +0200

    add path default to home

--- main.go
+++ main.go
@@ -12,11 +12,17 @@ func main() {
 }
 
 func setupRoutes() {
+	//make a home handler function
+	http.HandleFunc("/", homeHandler)
 	http.HandleFunc("/users", createUserHandler)
 	http.HandleFunc("/users/{id}", deleteUserHandler)
 	http.HandleFunc("/rooms", createRoomHandler)
 	http.HandleFunc("/rooms/{id}", deleteRoomHandler)
 	http.HandleFunc("/rooms/{id}/messages", sendMessageHandler)
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Welcome to my chat API")
 }
 
 func createUserHandler(w http.ResponseWriter, r *http.Request) {
