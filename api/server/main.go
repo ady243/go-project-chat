a1520af183d6a0b337ccb298e4d9ba165f3f0e2d
Ady Masivi <adymasivi@Air-de-Ady.lan>
2023-07-08 13:38:06 +0200

    trying port server and router

--- api/server/main.go
+++ api/server/main.go
@@ -4,12 +4,22 @@ import (
 	"log"
 
 	"github.com/ady243/go-project-chat/db"
+	"github.com/ady243/go-project-chat/router"
+	"github.com/ady243/go-project-chat/user"
 )
 
 func main() {
-	_, err := db.NewDB()
+
+	db, err := db.NewDB()
 	if err != nil {
 		log.Fatal("Error connecting to database: ", err)
 	}
+
+	userRepo := user.NewRepository(db.GetDB())
+	userSrvc := user.NewService(userRepo)
+	userHandler := user.Newhandler(userSrvc)
+
+	router.InitRoutes(userHandler)
+	router.Start(":8080")
 
 }
